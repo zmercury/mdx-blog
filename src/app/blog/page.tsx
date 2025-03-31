312f541ca58aad075310949bd4fa2c910d5f721e
zmercury <messiniki@outlook.com>
2025-03-31 18:14:08 +0545

    change: blog list heading

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -12,9 +12,6 @@ export default async function BlogPage() {
 
   return (
     <div className="container mx-auto px-4 py-8">
-      <h1 className="mb-8 text-4xl font-bold text-gray-900 dark:text-white">
-        Blog Posts
-      </h1>
       <BlogList posts={posts} />
     </div>
   )
